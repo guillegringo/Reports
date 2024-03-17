7b3de4659df8099b94b10fd632bea3df73983582
guillegringo <guillermo.wortmann@gmail.com>
2024-03-17 09:55:46 -0300

    add component scan config to load beans into the worker

--- Worker/src/main/java/com/example/report/worker/JobServerApplication.java
+++ Worker/src/main/java/com/example/report/worker/JobServerApplication.java
@@ -3,11 +3,9 @@ package com.example.report.worker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
-
-import javax.smartcardio.CardPermission;
 
 @SpringBootApplication()
+@ComponentScan(basePackages = "com.example.report.service")
 public class JobServerApplication {
 
     public static void main(String[] args) {
