d4a58cc9380fab49a611604e5ab548880bf26c13
guillegringo <guillermo.wortmann@gmail.com>
2024-03-25 10:05:11 -0300

    refactor scheduling service to make a bulk enqueue to improve performance

--- Service/src/main/java/com/example/report/service/impl/ScheduleServiceImpl.java
+++ Service/src/main/java/com/example/report/service/impl/ScheduleServiceImpl.java
@@ -1,17 +1,17 @@
 package com.example.report.service.impl;
 
 import com.example.report.service.ReportGeneratorService;
-import com.example.report.entity.Subscription;
 import com.example.report.mapper.ReportParamsMapper;
 import com.example.report.model.ReportParams;
+
 import com.example.report.util.PeriodUtils;
 import com.example.report.dao.SubscriptionRepository;
 import com.example.report.service.ScheduleService;
 import org.jobrunr.scheduling.JobScheduler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.stream.Stream;
 
-import java.util.List;
 @Service
 public class ScheduleServiceImpl implements ScheduleService {
 
@@ -24,17 +24,19 @@ public class ScheduleServiceImpl implements ScheduleService {
     @Autowired
     private SubscriptionRepository subscriptionRepository;
 
+
     @Override
-    public void scheduleDailyTasks(String intitutionId) {
+    public void scheduleDailyTasks(String institutionId) {
 
-        List<Subscription> subcriptions = subscriptionRepository.findDailySubscriptionsByInstitutionId(intitutionId);
+        Stream<ReportParams> paramsStream = subscriptionRepository.findDailySubscriptionsByInstitutionId(institutionId)
+                .stream()
+                .map(subscription -> {
+                    ReportParams reportParams = reportParamsMapper.subscriptionToReportParams(subscription);
+                    reportParams.setFrom(PeriodUtils.getStartOfToday());
+                    reportParams.setTo(PeriodUtils.getEndOfDay());
+                    return reportParams;
+                });
 
-        subcriptions.forEach(item -> {
-            ReportParams reportParams = reportParamsMapper.subscriptionToReportParams(item);
-            reportParams.setFrom(PeriodUtils.getStartOfToday());
-            reportParams.setTo(PeriodUtils.getEndOfDay());
-            jobScheduler.<ReportGeneratorService>enqueue(reportGeneratorService -> reportGeneratorService.generate(reportParams));
-        });
-
+        jobScheduler.<ReportGeneratorService, ReportParams>enqueue(paramsStream, ReportGeneratorService::generate);
     }
 }
